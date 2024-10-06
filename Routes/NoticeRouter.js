bbb0d3e3ea2c6aacacc306c1d539af36324fc8fd
unknown <rahul077@academiacollege.edu.np>
2024-10-06 14:21:26 +0545

    frontend url

--- Routes/NoticeRouter.js
+++ Routes/NoticeRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const {getNotice, CreateNotice, DeleteNotice,setShowNotice} = require('../controllers/NoticeControllers');
-const { set } = require('mongoose');
 
 
 router.route('/').get(getNotice).post(CreateNotice);
